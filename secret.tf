e0d9f45f39eee7f2383c5838c2c58d2913bdaf66
amitkumardube <37042351+amitkumardube@users.noreply.github.com>
2021-04-14 10:35:59 +0300

    Create secret.tf

---
+++ secret.tf
@@ -0,0 +1,15 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret-test"
+
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+    replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
