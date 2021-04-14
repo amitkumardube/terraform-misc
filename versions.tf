8f1eb170f547292699cea4d44990bda695e84df4
Amit Dube <amitkumardube@gmail.com>
2021-04-14 08:33:33 +0000

    fixed the config to create secret with CMEK

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@
 // This is called terraform settings block
 terraform {
   // to mention what terraform CLI version should process this module
-  required_version = ">=0.14"
+  //required_version = ">=0.14"
   // this blocks declares a google provider with specific version
   required_providers {
     google = {
@@ -11,7 +11,7 @@ terraform {
     }
   }
   // to use an experimental feature. These features are considered under experiment and can change in future releases. We need to explicitly tell Terraform if we need to use a experimental feature.
-  experiments = [provider_sensitive_attrs]
+  //experiments = [provider_sensitive_attrs]
 
   // to use a backend other than local. Local is default
   /*backend "gcs" {
