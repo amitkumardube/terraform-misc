e665cd716c337b1cbbb052b8e6f0dc82e2b463a3
amitkumardube <37042351+amitkumardube@users.noreply.github.com>
2021-04-16 23:53:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "gcloud" {
   source  = "terraform-google-modules/gcloud/google"
   version = "2.0.3"
   create_cmd_body = "secrets replication update secret-test --set-kms-key=projects/my-first-project-298218/locations/us-east1/keyRings/secret-manager-cmek/cryptoKeys/my-cmek-key --location=us-east1 --project my-first-project-298218"
-  service_account_key_file = "./my-first-project-298218-964978bfcdc4.json"
+  //service_account_key_file = "./my-first-project-298218-964978bfcdc4.json"
 }
