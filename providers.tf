f57cd5016768a54b9795060e3f7ea4e7f2fcf9fe
amitkumardube <amitkumardube@gmail.com>
2021-04-21 05:10:36 +0300

    fixed the key file

--- providers.tf
+++ providers.tf
@@ -2,5 +2,5 @@ provider "google" {
     project = var.provider_config["project_id"]
     region = var.provider_config["region"]
     zone = var.provider_config["zone"]
-    credentials = file("my-first-project-298218-964978bfcdc4.json")
+    credentials = file("my-first-project-298218-7e25767dcc93.json")
 }
