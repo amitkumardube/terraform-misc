6596ba09c023da589555f55357cc5043e5b2ae51
amitkumardube <amitkumardube@gmail.com>
2021-04-21 07:56:47 +0300

    fixed the key file

--- gcloud.tf
+++ gcloud.tf
@@ -4,6 +4,7 @@ resource "null_resource" "gcloud" {
       curl https://sdk.cloud.google.com > install.sh
       bash install.sh --disable-prompts
       gcloud auth activate-service-account --key-file=./my-first-project-298218-7e25767dcc93.json
+      gcloud secrets replication update secret-test --set-kms-key=projects/my-first-project-298218/locations/us-east1/keyRings/secret-manager-cmek/cryptoKeys/man-cmek-key --location=us-east1 --project my-first-project-298218
     EOH
   }
 }
