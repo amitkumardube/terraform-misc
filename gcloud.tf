3ba53737567149b227c050278e0610e6ab2a16e4
amitkumardube <amitkumardube@gmail.com>
2021-04-21 07:51:31 +0300

    fixed the key file

--- gcloud.tf
+++ gcloud.tf
@@ -3,7 +3,7 @@ resource "null_resource" "gcloud" {
       command = <<EOH
       curl https://sdk.cloud.google.com > install.sh
       bash install.sh --disable-prompts
-      gcloud auth activate-service-account ./my-first-project-298218-7e25767dcc93.json
+      gcloud auth activate-service-account --key-file=./my-first-project-298218-7e25767dcc93.json
     EOH
   }
 }
