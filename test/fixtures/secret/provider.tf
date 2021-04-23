e33cead5edd65a2db482d921b22a57b95ef67b4d
amitkumardube <amitkumardube@gmail.com>
2021-04-23 16:45:22 +0300

    Added test cases

--- test/fixtures/secret/provider.tf
+++ test/fixtures/secret/provider.tf
@@ -2,5 +2,5 @@ provider "google" {
     project = "my-first-project-298218"
     region = "us-central"
     zone = "us-central1-a"
-    credentials = file("my-first-project-298218-7e25767dcc93.json")
+    credentials = file("../../../my-first-project-298218-7e25767dcc93.json")
 }
