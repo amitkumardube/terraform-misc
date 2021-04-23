62dfc2c8370a6f9ca178a7b01b08768d190b43d1
amitkumardube <amitkumardube@gmail.com>
2021-04-23 16:42:38 +0300

    Added test cases

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
-provider "google" {
+/*provider "google" {
     project = var.provider_config["project_id"]
     region = var.provider_config["region"]
     zone = var.provider_config["zone"]
     credentials = file("my-first-project-298218-7e25767dcc93.json")
-}
+}*/
---
+++ test/fixtures/secret/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    project = "my-first-project-298218"
+    region = "us-central"
+    zone = "us-central1-a"
+    credentials = file("my-first-project-298218-7e25767dcc93.json")
+}
